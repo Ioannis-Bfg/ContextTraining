b236e4b3b8fd0d627ff3e7747d949b5e418b9fd1
Ioannis Boufogiannis <ioannisboufogiannis@gmail.com>
2024-08-02 14:05:35 +0300

    finished

--- src/components/display.jsx
+++ src/components/display.jsx
@@ -4,7 +4,7 @@ import { useContext } from "react";
 export default function Display() {
   const { displayValue } = useContext(DisplayContext);
   return (
-    <div className="bg-red-500 rounded-lg flex-1 text-white font-bold text-6xl flex justify-end items-end px-4 py-4 overflow-x-auto">
+    <div className="bg-[#f3e7d0] rounded-lg flex-1 text-zinc-700 font-bold text-6xl flex justify-end items-center px-4 py-4 overflow-x-auto">
       {displayValue}
     </div>
   );
--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -23,8 +23,11 @@ export default function Calculator() {
           setOperator,
         }}
       >
-        <div id="wrapper" className="flex py-4 justify-center items-center">
-          <div className="bg-black py-10 px-10 flex flex-col gap-8 text-sky-500  min-w-[30rem] max-w-[30rem] aspect-[9/16] rounded-md">
+        <div
+          id="wrapper"
+          className="flex py-4 justify-center h-screen items-center"
+        >
+          <div className="bg-[#181313] p-8  flex flex-col gap-8 text-sky-500  min-w-[30rem] max-w-[30rem] aspect-[9/16] rounded-md">
             <Display />
             <Numpad />
           </div>
